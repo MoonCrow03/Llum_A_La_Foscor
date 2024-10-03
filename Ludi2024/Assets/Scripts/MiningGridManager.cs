e9d80476c3160629d08a7bdae3a51a426946863c
Rubén Zuñiga Arancon <130458067+rubenzu03@users.noreply.github.com>
2024-10-04 00:06:17 +0200

    Grid generates on correct axis

--- Ludi2024/Assets/Scripts/MiningGridManager.cs
+++ Ludi2024/Assets/Scripts/MiningGridManager.cs
@@ -1,16 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
 using System.Runtime.Serialization.Formatters;
 using Assets.Scripts;
 using UnityEngine;
+using Quaternion = UnityEngine.Quaternion;
+using Vector3 = UnityEngine.Vector3;
 
 public class MiningGridManager : MonoBehaviour, IGrid
 {
+    [Header("Grid Settings")]
     [SerializeField] private int _width;
     [SerializeField] private int _height;
     [SerializeField] private int _numberOfItems;
     [SerializeField] private int _miningDepth;
 
+    [Header("Prefabs")]
     [SerializeField] private GameObject _renderingGrid;
     [SerializeField] private MiningTile _emptyMiningTilePrefab;
     [SerializeField] private List<MiningItem> _miningItems;
@@ -20,27 +25,25 @@ public class MiningGridManager : MonoBehaviour, IGrid
         GenerateGrid();
     }
 
-    void Update()
-    {
-        
-    }
-
     public void GenerateGrid()
     {
         for (int i = 0; i < _miningDepth; i++)
         {
             Debug.Log("Generating Layer " + _miningDepth);
-            GenerateLayer(_miningDepth);
+            GenerateLayer(i);
         }
     }
 
     void GenerateLayer(int miningDepth)
     {
+        Vector3 gridOrigin = _renderingGrid.transform.position;
         for (int i = 0; i < _width; i++)
         {
             for (int j = 0; j < _height; j++)
             {
-                var generatedMiningTile = Instantiate(_emptyMiningTilePrefab, new Vector3(i, j, miningDepth), Quaternion.identity);
+                Vector3 tilePosition = new Vector3(gridOrigin.x + i, gridOrigin.y - miningDepth, gridOrigin.z - j);
+                var generatedMiningTile = Instantiate(_emptyMiningTilePrefab, tilePosition, Quaternion.identity);
+                generatedMiningTile.transform.SetParent(_renderingGrid.transform);
             }
         }
     }
