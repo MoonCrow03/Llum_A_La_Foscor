3a3618526de0294a721e2b63831857ff5ad5f39a
MoonCrow03 <juliating03@hotmail.com>
2024-10-04 12:12:13 +0200

    Basic grid gen

---
+++ Ludi2024/Assets/Scripts/Puzzle/PuzzlePiece.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzlePiece : MonoBehaviour
+{
+    [Header("Tile settings")]
+    [SerializeField] private float _width;
+    [SerializeField] private float _height;
+    [SerializeField] private Vector2Int _correctIndex;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
