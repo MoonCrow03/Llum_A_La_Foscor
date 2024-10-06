2b2b97f19ca3316f2801d6f92691407e92a64770
Rubén Zuñiga Arancon <130458067+rubenzu03@users.noreply.github.com>
2024-10-06 21:28:14 +0200

    FPS Counter implemented

---
+++ Ludi2024/Assets/Scripts/Utilities/FPSCounter.cs
@@ -0,0 +1,35 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace Utilities
+{
+    public class FPSCounter : MonoBehaviour
+    {
+        public TextMeshProUGUI fpsText;
+
+        private const float PollingTime = 1f;
+        private float time;
+        private int frameCount;
+
+        private void Update()
+        {
+            time += Time.deltaTime;
+            frameCount++;
+
+            if (!(time >= PollingTime)) return;
+            int frameRate = Mathf.RoundToInt(frameCount / time);
+            fpsText.text = frameRate + " FPS";
+
+            fpsText.color = frameRate switch
+            {
+                >= 60 => Color.green,
+                >= 30 => Color.yellow,
+                _ => Color.red
+            };
+
+            time -= PollingTime;
+            frameCount = 0;
+        }
+    }
+}
