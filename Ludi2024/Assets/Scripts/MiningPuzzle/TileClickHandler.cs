b4ed2785788885bbeb9756734016ea77d1e4a626
Rubén Zuñiga Arancon <130458067+rubenzu03@users.noreply.github.com>
2024-10-12 00:55:26 +0200

    Reset TileClickHandler

--- Ludi2024/Assets/Scripts/MiningPuzzle/TileClickHandler.cs
+++ Ludi2024/Assets/Scripts/MiningPuzzle/TileClickHandler.cs
@@ -6,38 +6,9 @@ namespace MiningPuzzle
 {
     public class TileClickHandler : MonoBehaviour, IPointerClickHandler
     {
-        public MiningTileType TileType { get; set; }
-        public MiningItem Item { get; set; }
-
-        private MiningGridManager gridManager;
-        private Vector3Int gridPosition;
-
-        private void Awake()
-        {
-            gridManager = MiningGridManager.Instance;
-        }
-
-        public void Initialize(Vector3Int position)
-        {
-            gridPosition = position;
-        }
-
         public void OnPointerClick(PointerEventData eventData)
         {
-            if (TileType == MiningTileType.Item)
-            {
-                // Perform action for item tile
-            }
-            else if (TileType == MiningTileType.Empty)
-            {
-                // Perform action for empty tile
-                gridManager.SetGridPositionToAir(gridPosition);
-                if (gridManager.CheckIfAllItemsAreDiscovered())
-                {
-                    Debug.LogWarning("All items discovered!");
-                }
-                Destroy(gameObject);
-            }
+            throw new NotImplementedException();
         }
     }
 }
