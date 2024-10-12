ce4659c6158491cfb6bd5618c33aa6804c0f0e13
Rubén Zuñiga Arancon <130458067+rubenzu03@users.noreply.github.com>
2024-10-12 10:49:52 +0200

    Grid generates correctly with assets

--- Ludi2024/Assets/Scripts/MiningPuzzle/MiningGridManager.cs
+++ Ludi2024/Assets/Scripts/MiningPuzzle/MiningGridManager.cs
@@ -8,24 +8,63 @@ namespace MiningPuzzle
 {
     public class MiningGridManager : MonoBehaviour, IGrid
     {
-        [Header("Grid Settings")]
-        [SerializeField] private int width;
-        [SerializeField] private int height;
-        [SerializeField] private int numberOfItems;
-        [SerializeField] private int miningDepth;
+        public int width;
+        public int height;
+        public int numberOfItems;
+        public int miningDepth;
 
         [Header("Prefabs")]
-        [SerializeField] private GameObject renderingGrid;
-        [SerializeField] private List<MiningItem> miningItems;
-        [SerializeField] private List<GameObject> emptyMiningTilePrefabs;
+        public GameObject renderingOrigin;
+        public List<MiningItem> miningItems;
+        public List<GameObject> emptyMiningTilePrefabs;
         
         [Header("Camera")]
-        [SerializeField] private Camera mainCamera;
+        public Camera mainCamera;
 
+
+        private void Awake()
+        {
+            GenerateGrid();
+        }
 
         public void GenerateGrid()
         {
-            
+            for (int x = 1; x <= width; x++)
+            {
+                for (int z = 1; z <= height; z++)
+                {
+                    for (int y = 1; y <= miningDepth; y++)
+                    {
+                        Vector3 position = new Vector3(x * GetTileWidth(), y , z * GetTileHeight()) + renderingOrigin.transform.position;
+                        GameObject tilePrefab = GetTileBasedOnDepth(y);
+                        Quaternion rotation = Quaternion.Euler(-90, 0, 0);
+                        Instantiate(tilePrefab, position, rotation);
+                    }
+                }
+            }
+        }
+
+        private float GetTileWidth()
+        {
+            return emptyMiningTilePrefabs[0].GetComponent<Renderer>().bounds.size.x;
+        }
+        
+        private float GetTileHeight()
+        {
+            return emptyMiningTilePrefabs[0].GetComponent<Renderer>().bounds.size.z;
+        }
+
+        private GameObject GetTileBasedOnDepth(int z)
+        {
+            return z switch
+            {
+                1 => emptyMiningTilePrefabs[0],
+                2 => emptyMiningTilePrefabs[1],
+                3 => emptyMiningTilePrefabs[2],
+                4 => emptyMiningTilePrefabs[3],
+                5 => emptyMiningTilePrefabs[4],
+                _ => emptyMiningTilePrefabs[0]
+            };
         }
     }
 }
