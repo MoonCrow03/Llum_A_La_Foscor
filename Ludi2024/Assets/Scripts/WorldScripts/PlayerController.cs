cd5aed109fb3f21cf059ce1d67b6537575079528
Rubén Zuñiga Arancon <130458067+rubenzu03@users.noreply.github.com>
2024-10-12 20:58:05 +0200

    Migrated world movement to new input system

--- Ludi2024/Assets/Scripts/WorldScripts/PlayerController.cs
+++ Ludi2024/Assets/Scripts/WorldScripts/PlayerController.cs
@@ -9,7 +9,7 @@ public class PlayerController : MonoBehaviour
     
     private Vector3 moveDirection = Vector3.zero;
     private CharacterController controller;
-
+    
     private void Start()
     {
         controller = GetComponent<CharacterController>();
@@ -17,11 +17,15 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
-        
-        moveDirection = new Vector3(moveHorizontal, 0.0f, moveVertical);
-        moveDirection *= speed;
+        Vector2 input = InputManager.Instance.MovementInput;
+        if (input != Vector2.zero)
+        {
+            moveDirection = new Vector3(input.x, 0, input.y) * speed;
+        }
+        else
+        {
+            moveDirection = Vector3.zero;
+        }
         
         controller.Move(moveDirection * Time.deltaTime);
     }
