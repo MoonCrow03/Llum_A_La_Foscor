f6acd54d8f06a82aa7c2712994d9926b5ae42935
MoonCrow03 <juliating03@hotmail.com>
2024-10-13 18:35:29 +0200

    Fix

---
+++ Ludi2024/Assets/Scripts/ConnectWords/LineController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineController : MonoBehaviour
+{
+    private Transform[] m_Points;
+    private LineRenderer m_LineRenderer;
+
+    private void Awake()
+    {
+        m_LineRenderer = GetComponent<LineRenderer>();
+    }
+
+    private void Update()
+    {
+        for (int i = 0; i < m_Points.Length; i++)
+        {
+            m_LineRenderer.SetPosition(i, m_Points[i].position);
+        }
+    }
+
+    public void SetPoints(Transform[] p_Points)
+    {
+        m_Points = p_Points;
+        m_LineRenderer.positionCount = m_Points.Length;
+    }
+}
