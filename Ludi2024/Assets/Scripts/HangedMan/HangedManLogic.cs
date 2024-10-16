e01ff06aafb51174fd3387167db5e6b2af6c6a14
Rubén Zuñiga Arancon <130458067+rubenzu03@users.noreply.github.com>
2024-10-16 23:21:52 +0200

    Hanged Man partially working

--- Ludi2024/Assets/Scripts/HangedMan/HangedManLogic.cs
+++ Ludi2024/Assets/Scripts/HangedMan/HangedManLogic.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.Serialization;
+using UnityEngine.UI;
 using Random = System.Random;
 
 namespace HangedMan
@@ -12,8 +14,15 @@ namespace HangedMan
         [SerializeField] private List<string> wordList;
         [SerializeField] private int maxGuesses;
         
+        [Header("Canvas Settings")]
+        [SerializeField] private GameObject letterButtonPrefab;
+        [SerializeField] private Transform letterParent;
+        [SerializeField] private GameObject letterFromGuessWordPrefab;
+        [SerializeField] private Transform letterFromGuessWordParent;
+        
         private string wordToGuess;
         private Dictionary<char, bool> availableLetters;
+        private Dictionary<char, GameObject> letterObjects;
 
         private static readonly List<char> letters = new List<char>
         {
@@ -25,11 +34,45 @@ namespace HangedMan
         private void Awake()
         {
             availableLetters = new Dictionary<char, bool>();
+            letterObjects = new Dictionary<char, GameObject>();
         }
 
         private void Start()
         {
             SelectRandomWord();
+            GenerateEmptyLetters();
+            GenerateLetters();
+        }
+
+        private void GenerateLetters()
+        {
+            foreach (var letter in letters)
+            {
+                GameObject obj = Instantiate(letterButtonPrefab, letterParent);
+                obj.GetComponentInChildren<TextMeshProUGUI>().text = letter.ToString();
+                Button button = obj.GetComponentInChildren<Button>();
+                char letterCopy = letter;
+                button.onClick.AddListener(() => OnLetterClicked(letterCopy));
+                letterObjects.Add(letter, obj);
+                availableLetters.Add(letter, true);
+            }
+        }
+        
+        private void GenerateEmptyLetters()
+        {
+            for (int i = 0; i < wordToGuess.Length; i++)
+            {
+                GameObject obj = Instantiate(letterFromGuessWordPrefab, letterFromGuessWordParent);
+                obj.GetComponentInChildren<TextMeshProUGUI>().text = "_";
+            }
+        }
+        
+        public void OnLetterClicked(char letterClicked)
+        {
+            if (availableLetters[letterClicked])
+            {
+                GuessLetter(letterClicked);
+            }
         }
 
         private void SelectRandomWord()
@@ -39,15 +82,28 @@ namespace HangedMan
 
         private void GuessLetter(char letterGuessed)
         {
-            availableLetters[letterGuessed] = true;
+            availableLetters[letterGuessed] = false;
+            DisableLetterInteraction(letterGuessed);
+            
             if (wordToGuess.Contains(letterGuessed))
             {
                 UpdateShownWord(letterGuessed);
             }
             else
             {
+                ChangeLetterColor(letterGuessed, Color.red);
                 maxGuesses--;
             }
+        }
+
+        private void DisableLetterInteraction(char letterGuessed)
+        {
+            letterObjects[letterGuessed].GetComponentInChildren<Button>().interactable = false;
+        }
+
+        private void ChangeLetterColor(char letterGuessed, Color color)
+        {
+            letterObjects[letterGuessed].GetComponentInChildren<TextMeshProUGUI>().color = color;
         }
 
         private void UpdateShownWord(char letterGuessed)
@@ -56,7 +112,8 @@ namespace HangedMan
             {
                 if (wordToGuess[i] == letterGuessed)
                 {
-                    // Update shown word
+                    Transform letter = letterFromGuessWordParent.GetChild(i);
+                    letter.GetComponentInChildren<TextMeshProUGUI>().text = letterGuessed.ToString();
                 }
             }
         }
