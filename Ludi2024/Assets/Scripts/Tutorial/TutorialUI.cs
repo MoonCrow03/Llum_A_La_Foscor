638e0e0006f6314762da721e4917bc1854e8a160
MoonCrow03 <juliating03@hotmail.com>
2024-10-26 19:38:22 +0200

    fix and refractor

--- Ludi2024/Assets/Scripts/Tutorial/TutorialUI.cs
+++ Ludi2024/Assets/Scripts/Tutorial/TutorialUI.cs
@@ -13,105 +13,7 @@ public class TutorialUI : MonoBehaviour
     [SerializeField] private Animator m_Animator;
 
     [Header("Settings")]
-    [SerializeField] private int m_MaxPages = 2;
-
-    private int m_CurrentIndex;
-    private bool m_IsNoteBookEnabled;
-
-    private List<string> m_Pages;
-    private List<AnimationClip> m_Clips;
-
-    private void Start()
-    {
-        m_Clips = m_Animator.runtimeAnimatorController.animationClips.ToList();
-
-        m_Pages = new List<string>();
-
-        m_IsNoteBookEnabled = true;
-        m_CurrentIndex = 0;
-    }
-
-    private void Update()
-    {
-        if (!m_IsNoteBookEnabled) return;
-
-        if (InputManager.Instance.Q.Tap)
-        {
-            Debug.Log("Previous Page");
-            PreviousPage();
-        }
-
-        if (InputManager.Instance.E.Tap)
-        {
-            Debug.Log("Next Page");
-            NextPage();
-        }
-    }
-
-    private void NextPage()
-    {
-        if (!SetNextIndex()) return;
-
-        ClearBulletPoints();
-        StartCoroutine(HideText("NextPage"));
-
-        PassPage();
-    }
-
-    private void PreviousPage()
-    {
-        if (!SetPreviousIndex()) return;
-
-        ClearBulletPoints();
-        StartCoroutine(HideText("PrevPage"));
-
-        PassPage();
-    }
-
-    private bool SetNextIndex()
-    {
-        int l_index = m_CurrentIndex;
-
-        if (l_index + m_MaxPages < m_Pages.Count - 1)
-        {
-            m_CurrentIndex++;
-            return true;
-        }
-
-        return false;
-    }
-
-    private IEnumerator HideText(string p_trigger)
-    {
-        m_Animator.SetTrigger(p_trigger);
-        m_Text.gameObject.SetActive(false);
-
-        yield return new WaitForSeconds(m_Clips.Find(clip => clip.name.Equals("Armature|NextPage")).length);
-
-        m_Text.gameObject.SetActive(true);
-    }
-
-    private bool SetPreviousIndex()
-    {
-        int l_index = m_CurrentIndex;
-
-        if (l_index - m_MaxPages >= 0)
-        {
-            m_CurrentIndex--;
-            return true;
-        }
-        return false;
-    }
-
-    private void ClearBulletPoints()
-    {
-        m_Text.text = "";
-    }
-
-    private void PassPage()
-    {
-        m_Text.text = m_Pages[m_CurrentIndex];
-    }
+    [SerializeField] private bool m_IsNoteBookEnabled;
 
     private void OnDisableTutorialNoteBook()
     {
@@ -133,6 +35,8 @@ public class TutorialUI : MonoBehaviour
 
     private void PassPageAnimation()
     {
+        if(!m_IsNoteBookEnabled) return;
+
         m_Animator.SetTrigger("NextPage");
     }
 }
