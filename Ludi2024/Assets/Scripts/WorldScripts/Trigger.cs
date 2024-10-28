5858cb63d994a81902778db112b36355808eb4f1
Javier J. <135336325+JavierJ-TC@users.noreply.github.com>
2024-10-29 00:04:08 +0100

    Sombras & staff

--- Ludi2024/Assets/Scripts/WorldScripts/Trigger.cs
+++ Ludi2024/Assets/Scripts/WorldScripts/Trigger.cs
@@ -8,19 +8,36 @@ namespace WorldScripts
     {
         public List<Transform> spawnLocations;
         public Scenes sceneToLoad;
-        
+
+        [SerializeField] SpriteRenderer interactkeyUI;
+
+        private void Start()
+        {
+            interactkeyUI = GameObject.Find("FkeyInteract").GetComponent<SpriteRenderer>();
+            interactkeyUI.enabled = false;
+        }
+
         private void OnTriggerStay(Collider other)
         {
             if (other.CompareTag("Player"))
             {
+                interactkeyUI.enabled = true;
+
                 if (InputManager.Instance.F.Tap)
                 {
                     GameEvents.TriggerSetPlayerPosition();
                     GameManager.Instance.LoadScene(sceneToLoad);
+
                 }
             }
         }
 
-        
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                interactkeyUI.enabled = false;
+            }
+        }
     }
 }
