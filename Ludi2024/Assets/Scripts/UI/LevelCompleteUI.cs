848e3acf285b51e39cbb7f58e4ad889d4264690c
MoonCrow03 <juliating03@hotmail.com>
2024-10-28 23:07:11 +0100

    Fix player pos

--- Ludi2024/Assets/Scripts/UI/LevelCompleteUI.cs
+++ Ludi2024/Assets/Scripts/UI/LevelCompleteUI.cs
@@ -11,6 +11,8 @@ public class LevelCompleteUI : MonoBehaviour
     {
         GameManager.Instance.m_IsWorldCompleted = false;
         GameManager.Instance.m_IsTutorialCompleted = false;
+        GameManager.Instance.m_StartPosition = GameManager.Instance.m_OriginalPlayerPosition;
+        GameManager.Instance.m_StartRotation = GameManager.Instance.m_OriginalPlayerRotation;
         GameManager.Instance.LoadScene(m_NextScene);
     }
 }
