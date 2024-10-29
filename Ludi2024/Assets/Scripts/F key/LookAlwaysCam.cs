35aa5399b74bd6193d2f483881c63d66d29b71a2
Javier J. <135336325+JavierJ-TC@users.noreply.github.com>
2024-10-29 01:29:42 +0100

    Decals & F fix ahora es personal

--- Ludi2024/Assets/Scripts/F key/LookAlwaysCam.cs
+++ Ludi2024/Assets/Scripts/F key/LookAlwaysCam.cs
@@ -7,19 +7,9 @@ public class LookAlwaysCam : MonoBehaviour
 {
     //make the object always look at the camera
     [SerializeField] private Transform m_Camera;
-    [SerializeField] private GameManager m_GameManager;
-
-
-    private void Start()
-    {
-        m_GameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
-    }
-
+    
     void Update()
     {
-        if (m_GameManager != null && !m_GameManager.m_IsWorldCompleted)
-        {
-            transform.LookAt(m_Camera);
-        }
+        transform.LookAt(m_Camera);
     }
 }
